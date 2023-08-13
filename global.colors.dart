547db7ea3e42e8427c38f676fbc64289a910b5f1
MahinduBandaranayake <92430835+MahinduBandaranayake@users.noreply.github.com>
2023-08-13 17:43:41 +0530

    Add files via upload

---
+++ global.colors.dart
@@ -0,0 +1,7 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class GlobalColors {
+  static HexColor mainColor = HexColor('#1890FF');
+  static HexColor bodyColor = HexColor('#797979');
+  static HexColor textColor = HexColor('#4F4F4F');
+}
