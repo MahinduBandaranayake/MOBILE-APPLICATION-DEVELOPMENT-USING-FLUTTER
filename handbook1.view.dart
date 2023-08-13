d229e75b841e94114c2adc06279818eed930230b
MahinduBandaranayake <92430835+MahinduBandaranayake@users.noreply.github.com>
2023-08-13 17:47:49 +0530

    Add files via upload

---
+++ handbook1.view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HandPage extends StatelessWidget {
+  const HandPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
